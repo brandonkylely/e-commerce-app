a43cd76055a517683bebf7c413a8eb5de9ad1c7b
Brandon <brandonkly@ucla.edu>
2023-01-07 18:29:18 -0800

    added category route

--- db/schema.sql
+++ db/schema.sql
@@ -1,38 +1,38 @@
 DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
-USE ecommerce_db;
+-- USE ecommerce_db;
 
-CREATE TABLE categories (
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-category_name VARCHAR(100) NOT NULL
-);
+-- CREATE TABLE categories (
+-- id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- category_name VARCHAR(100) NOT NULL
+-- );
 
-CREATE TABLE products (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
-    price DECIMAL NOT NULL,
-    stock INT NOT NULL DEFAULT 10,
-    category_id INT NULL,
-    FOREIGN KEY (category_id)
-    REFERENCES categories(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE products (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     product_name VARCHAR(100) NOT NULL,
+--     price DECIMAL NOT NULL,
+--     stock INT NOT NULL DEFAULT 10,
+--     category_id INT NULL,
+--     FOREIGN KEY (category_id)
+--     REFERENCES categories(id)
+--     ON DELETE SET NULL
+-- );
 
-CREATE TABLE tags (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(100) NULL
-);
+-- CREATE TABLE tags (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     tag_name VARCHAR(100) NULL
+-- );
 
-CREATE TABLE product_tags (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_id INT NULL,
-    tag_id INT NULL,
-    FOREIGN KEY (product_id)
-    REFERENCES products(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (tag_id)
-    REFERENCES tags(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE product_tags (
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     product_id INT NULL,
+--     tag_id INT NULL,
+--     FOREIGN KEY (product_id)
+--     REFERENCES products(id)
+--     ON DELETE SET NULL,
+--     FOREIGN KEY (tag_id)
+--     REFERENCES tags(id)
+--     ON DELETE SET NULL
+-- );
 
